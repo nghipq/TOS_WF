43f8fedd449b89c8fd7db46b5f8d20d04dd7ac90
NganHua <NganHTCE140552@fpt.edu.vn>
2020-11-09 13:25:37 +0700

    booking

---
+++ TOS_WF/TOS_WF/Booking.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TOS_WF
+{
+    partial class Booking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Booking));
+            this.pnlHeader = new DevExpress.XtraEditors.PanelControl();
+            this.pnlFooter = new DevExpress.XtraEditors.PanelControl();
+            this.pnlLeft = new DevExpress.XtraEditors.PanelControl();
+            this.pnlRight = new DevExpress.XtraEditors.PanelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlHeader)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlFooter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pnlHeader.ContentImage = ((System.Drawing.Image)(resources.GetObject("pnlHeader.ContentImage")));
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1104, 88);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pnlFooter.ContentImage = ((System.Drawing.Image)(resources.GetObject("pnlFooter.ContentImage")));
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFooter.Location = new System.Drawing.Point(0, 544);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(1104, 88);
+            this.pnlFooter.TabIndex = 1;
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.Appearance.BackColor = System.Drawing.Color.White;
+            this.pnlLeft.Appearance.Options.UseBackColor = true;
+            this.pnlLeft.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pnlLeft.ContentImage = ((System.Drawing.Image)(resources.GetObject("pnlLeft.ContentImage")));
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 88);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(124, 456);
+            this.pnlLeft.TabIndex = 2;
+            // 
+            // pnlRight
+            // 
+            this.pnlRight.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pnlRight.ContentImage = ((System.Drawing.Image)(resources.GetObject("pnlRight.ContentImage")));
+            this.pnlRight.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pnlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlRight.Location = new System.Drawing.Point(980, 88);
+            this.pnlRight.Name = "pnlRight";
+            this.pnlRight.Size = new System.Drawing.Size(124, 456);
+            this.pnlRight.TabIndex = 3;
+            // 
+            // Booking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 632);
+            this.Controls.Add(this.pnlRight);
+            this.Controls.Add(this.pnlLeft);
+            this.Controls.Add(this.pnlFooter);
+            this.Controls.Add(this.pnlHeader);
+            this.IsMdiContainer = true;
+            this.Name = "Booking";
+            this.Text = "Booking";
+            ((System.ComponentModel.ISupportInitialize)(this.pnlHeader)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlFooter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlRight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl pnlHeader;
+        private DevExpress.XtraEditors.PanelControl pnlFooter;
+        private DevExpress.XtraEditors.PanelControl pnlLeft;
+        private DevExpress.XtraEditors.PanelControl pnlRight;
+    }
+}
