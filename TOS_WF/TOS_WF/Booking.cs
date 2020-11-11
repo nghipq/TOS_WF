275d9c1adce521ee1ef54e41da930a7d5d0b2389
NganHua <NganHTCE140552@fpt.edu.vn>
2020-11-11 12:17:02 +0700

    createbill

--- TOS_WF/TOS_WF/Booking.cs
+++ TOS_WF/TOS_WF/Booking.cs
@@ -14,10 +14,9 @@ namespace TOS_WF
 {
     public partial class Booking : DevExpress.XtraEditors.XtraForm
     {
-        public frmRoom pnlTicket1;
-        //public Ticket pnlTicket;
-        public ConfirmTicket pnlConTicket;
+        //public frmRoom pnlTicket1;
         public Ticket pnlTicket;
+        public ConfirmTicket pnlConTicket;
         public string cinemaName = "Vincom";
         public string schedule = "8.00 - 11.00";
         public string room = "3";
@@ -31,24 +30,26 @@ namespace TOS_WF
             //this.TopMost = true;
             //this.FormBorderStyle = FormBorderStyle.Fixed3D;
             //this.WindowState = FormWindowState.Maximized;
-            pnlTicket1 = new frmRoom();
-            pnlTicket1.MdiParent = this;
-            pnlTicket1.Show();
-            //pnlConTicket = new ConfirmTicket();
-            //pnlConTicket.MdiParent = this;
-            //pnlConTicket.btnConfirm.Click += new EventHandler(this.btnConfirm_Click);
-            //pnlConTicket.lblCinema.Text = cinemaName;
-            //pnlConTicket.lblSchedule.Text = schedule;
-            //pnlConTicket.lblRoom.Text = room;
-            //pnlConTicket.lblSeat.Text = seat;
-            //pnlConTicket.lblTotal.Text = total;
-            //pnlConTicket.Show();
+            //pnlTicket1 = new frmRoom();
+            //pnlTicket1.MdiParent = this;
+            //pnlTicket1.Show();
+            pnlConTicket = new ConfirmTicket();
+            pnlConTicket.MdiParent = this;
+            pnlConTicket.btnConfirm.Click += new EventHandler(this.btnConfirm_Click);
+            pnlConTicket.lblCinema.Text = cinemaName;
+            pnlConTicket.lblSchedule.Text = schedule;
+            pnlConTicket.lblRoom.Text = room;
+            pnlConTicket.lblSeat.Text = seat;
+            pnlConTicket.lblTotal.Text = total;
+            pnlConTicket.Show();
 
         }
 
         private void btnConfirm_Click(object sender, EventArgs e)
         {
-            
+            pnlTicket = new Ticket();
+            pnlTicket.MdiParent = this;
+            pnlTicket.Show();
         }
     }
 }
